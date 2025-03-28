95e938a0e2256590973f1d67c3c9c7471466d189
kamilnaja <kamilnaja@gmail.com>
2025-03-28 23:57:09 +0100

    refactor: use build_answer function for answer creation in questions

--- src/answer.rs
+++ src/answer.rs
@@ -2,3 +2,7 @@ pub struct Answer {
     pub text: String,
     pub is_correct: bool,
 }
+
+pub fn build_answer(text: String, is_correct: bool) -> Answer {
+    Answer { text, is_correct }
+}
--- src/create_questions.rs
+++ src/create_questions.rs
@@ -1,4 +1,4 @@
-use crate::answer::Answer;
+use crate::answer::build_answer;
 use crate::question::build_question;
 use crate::question::Question;
 
@@ -7,40 +7,22 @@ pub fn create_questions() -> Vec<Question> {
         build_question(
             String::from("Czy ryba to zwierze?"),
             vec![
-                Answer {
-                    text: String::from("Tak"),
-                    is_correct: true,
-                },
-                Answer {
-                    text: String::from("Nie"),
-                    is_correct: false,
-                },
+                build_answer(String::from("Tak"), true),
+                build_answer(String::from("Nie"), false),
             ],
         ),
         build_question(
             String::from("Jakiego koloru jest trawa"),
             vec![
-                Answer {
-                    text: String::from("Zielona"),
-                    is_correct: true,
-                },
-                Answer {
-                    text: String::from("Czerwona"),
-                    is_correct: false,
-                },
+                build_answer(String::from("Zielona"), true),
+                build_answer(String::from("Czerwona"), false),
             ],
         ),
         build_question(
             String::from("Ile nóg ma pająk"),
             vec![
-                Answer {
-                    text: String::from("4"),
-                    is_correct: false,
-                },
-                Answer {
-                    text: String::from("8"),
-                    is_correct: true,
-                },
+                build_answer(String::from("4"), false),
+                build_answer(String::from("8"), true),
             ],
         ),
     ]
